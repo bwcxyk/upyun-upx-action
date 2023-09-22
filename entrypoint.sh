401437a7c42e6c683b4c57f797aa8a6c8640725a
不忘初心 <1624717079@qq.com>
2023-09-22 17:22:03 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,12 +13,12 @@ apk update \
 upx login ${bucket} ${operator} ${operator_password}
 
 # 删除所有文件
-upx cd ${remote_path}
-upx rm -a || exit 0
-echo "upx rm successful!"
+# upx cd ${remote_path}
+# upx rm -a || exit 0
+# echo "upx rm successful!"
 # 上传文件
-upx upload -all --remote ${remote_path} ${local_path}/*
+# upx upload -all --remote ${remote_path} ${local_path}/*
 # 增量同步文件
-# ./upx sync ${local_path} ${remote_path}
+./upx sync -q ${local_path} ${remote_path}
 echo "upx upload successful!"
 upx logout
